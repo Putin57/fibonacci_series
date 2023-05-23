529af1d02c67e8239fb75dd2ee9f86d5f4813d58
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-05-23 11:45:46 +0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,15 @@
+import sys
+sys.stdin.reconfigure(line_buffering=True)
 num=int(input("Enter your fibonacci number : "))
 def fib(n):
 	a,b=0,1
-	if n==1:
-		print(a)
+	if n==1:print(a)
 	else:
-		print(a)
-		print(b)
+		print(a,end=" ")
+		print(b,end=" ")
 		for i in range(2,n):
 			c=a+b
 			a=b
 			b=c
-			print(c)
+			print(c,end=" ")
 fib(num)
